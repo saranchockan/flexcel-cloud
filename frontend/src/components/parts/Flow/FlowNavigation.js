0355ea8232e57ae3af1ab45cec2a2654e3c1e4f9
Saran Chockan <saranjitharunesh@gmail.com>
2020-07-02 08:28:08 -0500

    switched from navs to tabs

--- frontend/src/components/parts/Flow/FlowNavigation.js
+++ frontend/src/components/parts/Flow/FlowNavigation.js
@@ -1,38 +1,37 @@
 import React, { Component } from 'react'
 import Nav from 'react-bootstrap/Nav'
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab'
+
+// FlowNavigation contains the navigation tab and the hansontable flows
+// Functionality - add and delete tabs; renaming tabs, dragging tabs re-ordering
+
+// Flow data, tab data should be part of state - depends on how tab switching works
 
 export default class FlowNavigation extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            // Flow Data: Eithery empty or pulled from AWS -> Dashboard
-            flowData: ['AC', 'Framework', 'NC'],
+            flowTabNames: ['AC', 'Framework', 'NC'],
             currentFlowTabIndex: 0
         }
     }
 
     render() {
         return (
-            <div>
-                <Nav variant='pills' defaultActiveKey = {('link-' + this.state.currentFlowTabIndex)}>
-                    {
-                        this.state.flowData.map((value, index) => {
-                            return (
-                                <Nav.Item>
-                                    <Nav.Link
-                                        eventKey={('link-' + index)}
-                                    >
-                                        {this.state.flowData[index]}
-                                    </Nav.Link>
-                                </Nav.Item>
-                            )
-
-                        })
-                    }
-                </Nav>
-            </div>
-
+            // Sets up flow navigation tabs
+            <Tabs variant='pills' defaultActiveKey={('tab-' + this.state.currentFlowTabIndex)}>
+                {
+                    this.state.flowTabNames.map((value, index) => {
+                        return (
+                            <Tab eventKey={('tab-' + index)} title={this.state.flowTabNames[index]}>
+                                <h1> {this.state.flowTabNames[index]} </h1>
+                            </Tab>
+                        )
+                    })
+                }
+            </Tabs>
         )
     }
 }
--- frontend/src/components/screens/Flow.js
+++ frontend/src/components/screens/Flow.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import FlowNavigation from './../parts/Flow/FlowNavigation'
 
-
  // Flow screen setups the FlowNavigation and SpeechDoc. (Use Bootrap Grid)
  // Should have recieved flow data from AWS and propogate it to FlowNavigation.js through props
 
