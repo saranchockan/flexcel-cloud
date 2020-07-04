183b27ef6e3b24f0a58c25d0cdcda37c7b02dd1f
Saran Chockan <saranjitharunesh@gmail.com>
2020-07-03 23:59:45 -0500

    implemented responsive view of handsontable flow

--- frontend/src/components/parts/Flow/FlowNavigation.js
+++ frontend/src/components/parts/Flow/FlowNavigation.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import $ from 'jquery';
 import './../../../styling/Flow.css';
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
@@ -18,15 +19,34 @@ export default class FlowNavigation extends Component {
             currentFlowTabIndex: 0,
             flowTabNames: ['AC', 'Framework', 'NC'],
             currentFlowTabIndex: 0,
+            flowHeight: 500,
+            flowWidth: 500,
             flowSettings: {
                 colHeaders: true,
+                height: 500,
+                width: 500,
+                colWidths: 200,
+                minCols: 10,
+                minRows: 40,
                 licenseKey: 'non-commercial-and-evaluation',
-                stretchH: 'all', 
-                //preventOverflow: 'horizontal',
             },
             // Needs to be auto-generated i.e based of flowTab length? Use lambda, map?
             handsontableFlows: [React.createRef(), React.createRef(), React.createRef()]
         }
+    }
+    // Function executed when app is loaded
+    handleLoad = () => {
+        console.log('LOADED')
+        // ADD Loading Modal in here - so that Flexcel can setup height and data properly... (5 Seconds)
+        this.setCurrentFlowHeightAndWidth()
+        if(this.state.flowHeight != 500) this.updateAllHandsontableFlowHeightAndWidth()
+    }
+    // Function executed when windows is resized
+    handleResize = () => {
+        console.log('RESIZE')
+        this.setCurrentFlowHeightAndWidth()
+        this.updateAllHandsontableFlowHeightAndWidth()
+        this.renderCurrentHandsontableFlow()
     }
 
     // Renders the current active handsontable tab
@@ -34,8 +54,29 @@ export default class FlowNavigation extends Component {
     // and then render the handsontable so that it can display properly    
     renderCurrentHandsontableFlow = () => {
         setTimeout(() => {
-            this.state.handsontableFlows[this.state.currentFlowTabIndex].current.hotInstance.render()
+            var currentFlowHotInstance = this.state.handsontableFlows[this.state.currentFlowTabIndex].current.hotInstance
+            currentFlowHotInstance.render()
         }, 20)
+    }
+
+    // Updates the height off all handsontable flows
+    // to the current flow height
+    updateAllHandsontableFlowHeightAndWidth = () => {
+        for (var i = 0; i < this.state.handsontableFlows.length; i++) {
+            var hotInstance = this.state.handsontableFlows[i].current.hotInstance
+            hotInstance.updateSettings({ height: this.state.flowHeight, width: this.state.flowWidth })
+            hotInstance.render()
+        }
+    }
+
+    // Calculates flow height and width based off flow container height 
+    setCurrentFlowHeightAndWidth = () => {
+        // Error checking needed? Will .nav only return one component?
+        var flowNavigationContainerHeight = $('#flowNavigationContainer').height()
+        var navTabHeight = $('.nav').height()
+        var navTabWidth = $('.nav').width()
+        this.state.flowHeight = flowNavigationContainerHeight - navTabHeight;
+        this.state.flowWidth = navTabWidth
     }
 
     // Function executes everytime a tab is selected.
@@ -44,17 +85,19 @@ export default class FlowNavigation extends Component {
         console.log('Tab selected!')
         // Calculates current active tab index
         var tabIndex = parseInt(key.charAt(key.length - 1))
-
         this.state.currentFlowTabIndex = tabIndex
+
         this.renderCurrentHandsontableFlow()
     }
 
-    // Renders handsontable flow everytime screen is resized
+    // Adding onLoad() and onResize() listeners
     componentDidMount() {
-        window.addEventListener('resize', this.renderCurrentHandsontableFlow);
+        window.addEventListener('load', this.handleLoad);
+        window.addEventListener('resize', this.handleResize);
     }
     componentWillUnmount() {
-        window.removeEventListener('resize', this.renderCurrentHandsontableFlow);
+        window.addEventListener('load', this.handleLoad);
+        window.removeEventListener('resize', this.handleResize);
     }
 
     render() {
@@ -65,7 +108,7 @@ export default class FlowNavigation extends Component {
                     this.state.flowTabNames.map((value, index) => {
                         return (
                             <Tab eventKey={('tab-' + index)} title={value}>
-                                <div id = 'flowContainer'>
+                                <div id='flowContainer'>
                                     <FlexcelFlow ref={this.state.handsontableFlows[index]} settings={this.state.flowSettings} />
                                 </div>
                             </Tab>
