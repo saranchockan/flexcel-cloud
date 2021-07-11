ceb47b23027d4d8b13d363ecee1b2b627c2682b0
alexwenchen <47618496+alexwenchen@users.noreply.github.com>
2021-07-10 19:51:38 -0500

    Delete Desktop/flexcel-cloud-master directory

--- Desktop/flexcel-cloud-master/frontend/src/globals.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import jquery from 'jquery'
-
-window.React = React
-window.ReactDOM = ReactDOM
-window.jQuery = jquery
-window.jquery = jquery
-window.$ = jquery
