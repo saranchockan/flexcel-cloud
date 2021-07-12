5ba51c13dcb32083c4af05d8d8c28859259c9f01
Alex <alexchen3141@gmail.com>
2021-07-11 19:34:19 -0500

    fixed issue with auth0

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const domain = "flexcel-cloud.us.auth0.com";
+const clientId = "iRQD8MmLx0fRDOoFMFkfqMDPluCjdgAU";
 
 ReactDOM.render(
   <Auth0Provider
